f577575edadba1c5b73e590a947f12c70dec4ed6
Aarumugam Thanikasalam <Aaru7shan@gmail.com>
2014-08-22 12:48:27 +0530

    Check up
    Check out

---
+++ GitHubTest/GitHubTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GitHubTest
+//
+//  Created by Redcaso Solutions on 22/08/2014.
+//  Copyright (c) 2014 Redcaso Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
